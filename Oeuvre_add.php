9183f2628d4804718e315229c5bb3e81059da9b5
Neilerua <aureliendrey@gmail.com>
2018-03-25 14:00:08 +0200

    Add files created before start of git

---
+++ Oeuvre_add.php
@@ -0,0 +1,43 @@
+<?php
+include "connexion_bdd.php";
+//traitement
+
+if (isset($_POST["titre"])
+    AND isset($_POST["dateParution"])
+    AND isset($_POST['idAuteur'])){
+    //Controles des données
+    $donnees['titre'] = $_POST['titre'];
+    $donnees['dateParution'] = htmlentities($_POST['dateParution']);
+    $donnees['idAuteur'] = htmlentities($_POST['idAuteur']);
+
+    //acces au modele
+    $ma_requete_SQL ="INSERT INTO OEUVRE (noOeuvre,titre,dateParution, idAuteur)
+     VALUES (NULL,'".$donnees['titre']."','".$donnees['titre']."'
+     ,".$donnees['idAuteur'].");";
+
+    $bdd->exec($ma_requete_SQL);
+
+    header("Location: Produit_show.php");
+}
+
+//affichage de la vue
+
+?>
+
+<form>
+    <div class="row">
+        <fieldset>
+            <legend>Ajouter une Oeuvre</legend>
+            <label>Titre
+                <input name="titre" type="text" size="18" value=""/>
+            </label>
+            <label>Date de parution
+                <input name="dateParution" type="text" size="18" value=""/>
+            </label>
+            <label>identifiant de l'auteur
+                <input name="idAuteur" type="text" size="18" value="1"/>
+            </label>
+            <input type="submit" name="AddOeuvre" value="Ajouter une oeuvre">
+        </fieldset>
+    </div>
+</form>
---
+++ Oeuvre_show.php
@@ -0,0 +1,58 @@
+<?php
+//partie modele
+include("connexion_bdd.php");
+
+$ma_requete_SQL = "SELECT oe.titre, 
+oe.noOeuvre, 
+oe.idAuteur, 
+oe.dateParution
+FROM OEUVRE oe
+ORDER BY oe.titre";
+
+$reponse = $bdd->query($ma_requete_SQL);
+$donnees = $reponse->fetchAll();
+
+//partie vue
+?>
+
+<div class="row">
+    <a href="Oeuvre_add.php">Ajouter une oeuvre</a>
+    <table border="2">
+        <caption>Recapitulatifs des Oeuvres</caption>
+        <?php if(isset($donnees[0])): ?>
+        <thead>
+            <tr>
+                <th>titre de l'oeuvre</th>
+                <th>identifiant auteur</th>
+                <th>date de parution</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($donnees as $value):?>
+
+                <tr>
+                    <td>
+                        <?= $value['titre'] ?>
+                    </td>
+                    <td>
+                        <?= $value['idAuteur'] ?>
+                    </td>
+                    <td>
+                        <?= $value['dateParution'] ?>
+                    </td>
+                    <td>
+                        <a href="Oeuvre_edit.php?id=<?= $value['noOeuvre'] ?>">Modifier</a>
+                        <a href="Oeuvre_delete.php?id=<?= $value['noOeuvre'] ?>">Supprimer</a>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+        <?php else: ?>
+            <tr>
+                <td> Pas d'oeuvre dans la base de données</td>
+            </tr>
+        <?php endif; ?>
+    </table>
+</div>
+
+
