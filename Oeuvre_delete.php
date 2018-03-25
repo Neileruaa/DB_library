57a573c0827f8d99fdcfcec6e34756327cca7e42
Aurel <aureliendrey@gmail.com>
2018-03-25 16:50:33 +0200

    Adjust query for delete in Oeuvre_delete.php

--- Oeuvre_delete.php
+++ Oeuvre_delete.php
@@ -3,7 +3,11 @@ include "connexion_bdd.php";
 
 if (isset($_GET["id"])AND is_numeric($_GET["id"])) {
     $id = htmlentities($_GET['id']);
-    $ma_requete_SQL = "DELETE FROM OEUVRE WHERE noOeuvre=" . $id . ";";
+    $ma_requete_SQL =
+        "DELETE OEUVRE FROM OEUVRE
+INNER JOIN AUTEUR A ON OEUVRE.idAuteur = A.idAuteur
+WHERE noOeuvre =".$id.";";
+
     $bdd->exec($ma_requete_SQL);
 
     header("Location: Oeuvre_show.php");
